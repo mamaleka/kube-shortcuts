b9a3a6410a11b5f0b3ff05528b1dd90d441135ee
Mosima Damentt Mamaleka <31598092+mamaleka@users.noreply.github.com>
2024-09-24 14:28:32 +0000

    first batch of scripts

---
+++ export.sh
@@ -0,0 +1,18 @@
+#aliases
+alias k='kubectl'
+alias kr='kubectl run'
+alias kg='kubectl get'
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+alias kga='kubectl get all'
+alias kgaw='kubectl get all -o wide'
+
+alias kd='kubectl describe'
+alias kdes='kubectl describe'
+alias kdp='kubectl describe pod'
+alias 
+#exports
+export dr='--dry-run=client -o yaml > '
+export dryrun='--dry-run=client -o yaml > '
+export wide='-o wide'
+export w='-o wide'
